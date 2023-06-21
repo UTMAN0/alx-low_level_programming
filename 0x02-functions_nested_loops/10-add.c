a28f50e04b94bcf082e02e384c10fa2ecc13fe75
UTMAN0 <hidomi366@gmail.com>
2023-06-21 13:44:58 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - adds the two integeres.
+*	@n: This is the first parameter.
+*	@c: This is the second parameter.
+*	Return: a + b
+*/
+
+int add(int n, int c)
+{
+	return (n + c);
+}
