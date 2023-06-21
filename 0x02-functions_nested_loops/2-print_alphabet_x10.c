1b5af76fd200abb2b7f41193f64de119d736e8a5
UTMAN0 <hidomi366@gmail.com>
2023-06-21 11:30:47 -0700

    Task3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Is a function to print the alphabet x10
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+		char one;
+		int j;
+
+		two = 0;
+		while (two < 10)
+		{
+			for (one = 'a'; one <= 'z'; one++)
+			{
+			_putchar(one);
+			}
+			_putchar('\n');
+			two++;
+		}
+}
