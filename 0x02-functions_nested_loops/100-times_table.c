cb0aca961fa368b1a78564bb44156487d6a88364
UTMAN0 <hidomi366@gmail.com>
2023-06-21 14:19:57 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - this will rints the times table of the input,
+ * print_times_table - This will Print the times table of the input,
  *                     starting with 0.
- * @n: This is The value of the times table to be printed.
+ * @n: This is the value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-int o, a, r;/*o represents the first number in each multiplication
-							* operation, a represents the second number
-							* r represents the result of the multiplication.
+int x, y, z;/*x represents the first number in each multiplication
+							* operation, y represents the second number
+							* z represents the result of the multiplication.
 							*/
 
 	if (n >= 0 && n <= 15)
 	{
-		for (o = 0; o <= n; o++)
+		for (x = 0; x <= n; x++)
 		{
 			_putchar('0');
 
-			for (a = 1; a <= n; a++)
+			for (y = 1; y <= n; y++)
 			{
 				_putchar(',');
 				_putchar(' ');
 
-				r = o * a;
+				z = x * y;
 
-				if (r <= 99)
+				if (z <= 99)
 					_putchar(' ');
-				if (r <= 9)
+				if (z <= 9)
 					_putchar(' ');
 
-				if (r >= 100)
+				if (z >= 100)
 				{
-					_putchar((r / 100) + '0');
-					_putchar(((r / 10)) % 10 + '0');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10)) % 10 + '0');
 				}
-				else if (r <= 99 && r >= 10)
+				else if (z <= 99 && z >= 10)
 				{
-					_putchar((r / 10) + '0');
+					_putchar((z / 10) + '0');
 				}
-				_putchar((r % 10) + '0');
+				_putchar((z % 10) + '0');
 			}
 			_putchar('\n');
 		}
