6e63d2f3c81808025edfc008c02a215874763b6f
UTMAN0 <hidomi366@gmail.com>
2023-06-22 13:16:19 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - This is a function to draw a straight line.
+ *@n: This is The number of times the character _ should be printed.
+ * Return: Void
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	x = 0;
+	while (a < n)
+	{
+		_putchar('_');
+		x++;
+	}
+	_putchar('\n');
+}
