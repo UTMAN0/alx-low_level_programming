12820ca968135618851a48bb5f5b419966c4c6d8
UTMAN0 <hidomi366@gmail.com>
2023-06-22 15:20:21 -0700

    Files

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - This for tests function that prints if integer is positive or negative.
+* Return: 0.
+*/
+
+int main(void)
+{
+	int lite;
+
+	lite = 0;
+	positive_or_negative(lite);
+
+	return (0);
+}
