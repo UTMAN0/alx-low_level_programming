e80c0a15bc8381505d23ddc08fd0f0b851cbeee3
UTMAN0 <hidomi366@gmail.com>
2023-06-30 18:42:27 -0700

    fix Task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,19 +4,19 @@ int main(void)
 {
   int n;
   int a[5];
-  int *q;
+  int *p;
 
   a[2] = 1024;
-  q = &n;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
-   * - you are not allowed to modify q
+   * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(q + 5) = 98;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
