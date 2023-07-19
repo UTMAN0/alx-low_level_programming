a6a15a6ac2c82a5cd3d0e6f96752cbd4025835c4
UTMAN0 <hidomi366@gmail.com>
2023-07-19 14:17:49 -0700

    adv taskes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,42 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-    int bytes, i;
-    char *arr;
 
-    if (argc != 2)
-    {
-        printf("Error: Please provide the number of bytes\n");
-        exit(1);
-    }
+	/**
+	 * main - prints its own opcodes.
+	 * @argc: number of arguments.
+	 * @argv: array of arguments.
+	 *
+	 * Return: Always 0 (Success).
+	 */
+	int main(int argc, char *argv[])
+	{
+		int bytes, i;
+		char *arr;
 
-    bytes = atoi(argv[1]);
 
-    if (bytes <= 0)
-    {
-        printf("Error: Invalid number of bytes\n");
-        exit(2);
-    }
+		if (argc != 2)
+		{
+			printf("Error\n");
+			exit(1);
+		}
 
-    arr = (char *)main;
 
-    for (i = 0; i < bytes; i++)
-    {
-        printf("%02hhx", arr[i]);
-        if (i != bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
+		bytes = atoi(argv[1]);
 
-    return 0;
-}
+
+		if (bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+
+		arr = (char *)main;
+
+
+		for (i = 0; i < bytes; i++)
+		{
+			if (i == bytes - 1)
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
+			}
+			printf("%02hhx ", arr[i]);
+		}
+		return (0);
+	}
 
